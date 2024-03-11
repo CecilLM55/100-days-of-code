21ec6d9ce6bd8f1c3cbef4ed87b1647942057529
CecilLM55 <clopez16@ucol.mx>
2024-03-11 10:03:21 -0600

    day one of coding challenge

---
+++ month-one/week-1/day-one-years-to-days.js
@@ -0,0 +1,7 @@
+function yearsToDays() {
+    let years = prompt("Enter the number of years: ");
+    let days = years * 365;
+    alert(`${years} years is equal to ${days} days.`)
+}
+
+yearsToDays();
